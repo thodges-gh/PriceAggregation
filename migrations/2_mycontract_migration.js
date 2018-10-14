d58b8f6a6c1c1beafba8294b9649ff2942025b57
Thomas Hodges <me@thodges.com>
2018-10-14 08:22:33 -0500

    Update migration script

--- migrations/2_mycontract_migration.js
+++ migrations/2_mycontract_migration.js
@@ -5,10 +5,10 @@ var LinkToken = artifacts.require("LinkToken");
 module.exports = function(deployer, network) {
   switch (network) {
   case "ropsten":
-    deployer.deploy(MyContract, "0x20fE562d797A42Dcb3399062AE9546cd06f63280", "0x261a3F70acdC85CfC2FFc8badE43b1D42bf75D69");  
+    deployer.deploy(MyContract, "0x20fE562d797A42Dcb3399062AE9546cd06f63280");  
     break;
   case "rinkeby":
-    deployer.deploy(MyContract, "0x01BE23585060835E02B77ef475b0Cc51aA1e0709", "0xf18455e70984e8fda0ADbe2c8dD21509DBeFA218");
+    deployer.deploy(MyContract, "0x01BE23585060835E02B77ef475b0Cc51aA1e0709");
     break;
   default:
     deployer.deploy(LinkToken).then( function() {
